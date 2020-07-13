014e9a2f9ea054029d1985bd22c6633aeb62ad88
minjie <minjilei@umich.edu>
2020-07-13 16:17:38 -0400

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -47,8 +47,50 @@ class Venue(db.Model):
 
     shows = db.relationship('Show', backref='Venue', lazy=True)
 
+    '''
+    short()
+        short form representation of the venue
+    '''
+    def short(self):
+    	return {
+    		'id': self.id,
+    		'name': self.name,
+    		'genres': self.genres
+    	}
+
+    '''
+    long()
+        long form representation of the venue
+    '''
+    def long(self):
+    	return {
+    		'id': self.id,
+    		'name': self.name,
+    		'genres': self.genres,
+    		'city': self.city,
+    		'state': self.state,
+    		'address': self.address,
+    		'phone': self.phone,
+    		'website': self.website,
+    		'image_link': self.image_link,
+    		'facebook_link': self.facebook_link,
+    		'seeking_talent': self.seeking_talent,
+    		'seeking_description': self.seeking_description
+    	}
+
+    def insert(self):
+    	db.session.add(self)
+    	db.session.commit()
+
+    def delete(self):
+    	db.session.delete(self)
+    	db.session.commit()
+
+    def update(self):
+    	db.session.commit()
+
     def __repr__(self):
-      return f'<Venue {self.id} {self.name}>'
+    	return f'<Venue {self.id} {self.name}>'
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -67,13 +109,73 @@ class Artist(db.Model):
 
     shows = db.relationship('Show', backref='Artist', lazy=True)
 
+    '''
+    short()
+        short form representation of the venue
+    '''
+    def short(self):
+    	return {
+    		'id': self.id,
+    		'name': self.name,
+    		'genres': self.genres
+    	}
+
+    '''
+    long()
+        long form representation of the venue
+    '''
+    def long(self):
+    	return {
+    		'id': self.id,
+    		'name': self.name,
+    		'genres': self.genres,
+    		'city': self.city,
+    		'state': self.state,
+    		'phone': self.phone,
+    		'website': self.website,
+    		'image_link': self.image_link,
+    		'facebook_link': self.facebook_link,
+    		'seeking_venue': self.seeking_venue,
+    		'seeking_description': self.seeking_description
+    	}
+
+    def insert(self):
+    	db.session.add(self)
+    	db.session.commit()
+
+    def delete(self):
+    	db.session.delete(self)
+    	db.session.commit()
+
+    def update(self):
+    	db.session.commit()
+
     def __repr__(self):
-      return f'<Artist {self.id} {self.name}>'
+    	return f'<Artist {self.id} {self.name}>'
 
 class Show(db.Model):
-  __tablename__ = 'Show'
+	__tablename__ = 'Show'
 
-  id = db.Column(db.Integer, primary_key=True)
-  start_time =db.Column(db.DateTime, nullable=False)
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+	id = db.Column(db.Integer, primary_key=True)
+	start_time =db.Column(db.DateTime, nullable=False)
+	venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+	artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+
+	def format(self):
+		return {
+			'id': self.id,
+			'start_time': self.start_time,
+			'venue_id': self.venue_id,
+			'artist_id': self.artist_id
+		}
+
+	def insert(self):
+		db.session.add(self)
+		db.session.commit()
+
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()
+
+	def update(self):
+		db.session.commit()
