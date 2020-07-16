da622ee82945b4639103eafcf217027672e75f82
minjie <minjilei@umich.edu>
2020-07-16 18:51:48 -0400

    Revert "changed migration method"
    This reverts commit 47f35a2c8ff0319d847a2504e3540a8e187df7ad.

--- backend/models.py
+++ backend/models.py
@@ -22,7 +22,8 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    migrate = Migrate(app, db)
+    return db
 
 #----------------------------------------------------------------------------#
 # Models.
