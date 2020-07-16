47f35a2c8ff0319d847a2504e3540a8e187df7ad
minjie <minjilei@umich.edu>
2020-07-16 16:24:22 -0400

    changed migration method

--- backend/models.py
+++ backend/models.py
@@ -22,8 +22,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    migrate = Migrate(app, db)
-    return db
+    db.create_all()
 
 #----------------------------------------------------------------------------#
 # Models.
